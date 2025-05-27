5ef994ecd8bec4c112c75d39c82a67d5d2925f59
anurodh pancholi <pancholianurodh@gmail.com>
2025-05-27 13:48:52 +0530

    commit

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -11,21 +11,21 @@ const Testimonials = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-4xl mx-auto px-4 py-8">
 
-                {
-                    testimonialsData.map((item, index) => (
-                        <div key={index} className='bg-white rounded-xl p-6 drop-shadow-md max-w-lg m-auto hover:scale-105 transition-all duration-700'>
-                            <p className='text-4xl text-gray-500'>”</p>
-                            <p className='text-sm text-gray-500'>{item.text}</p>
-                            <div className='flex items-center gap-3 mt-5'>
-                                <img className='w-9 rounded-full' src={item.image} alt="" />
-                                <div>
-                                    <p className='text'>{item.author}</p>
-                                    <p className='text-sm text-gray-600'>{item.jobTitle}</p>
-                                </div>
-                            </div>
-                        </div>
+                {/* {
+                    // testimonialsData.map((item, index) => (
+                    //     <div key={index} className='bg-white rounded-xl p-6 drop-shadow-md max-w-lg m-auto hover:scale-105 transition-all duration-700'>
+                    //         <p className='text-4xl text-gray-500'>”</p>
+                    //         <p className='text-sm text-gray-500'>{item.text}</p>
+                    //         <div className='flex items-center gap-3 mt-5'>
+                    //             <img className='w-9 rounded-full' src={item.image} alt="" />
+                    //             <div>
+                    //                 <p className='text'>{item.author}</p>
+                    //                 <p className='text-sm text-gray-600'>{item.jobTitle}</p>
+                    //             </div>
+                    //         </div>
+                    //     </div>
                     ))
-                }
+                } */}
 
             </div>
         </div>
